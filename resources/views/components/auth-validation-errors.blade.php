917c4ea35f68250ba497e3219b2b0ed99fa233fb
ThomasBackers <thomasbackers@gmail.com>
2022-01-19 16:49:03 +0100

    repaired npm installation

--- resources/views/components/auth-validation-errors.blade.php
+++ resources/views/components/auth-validation-errors.blade.php
@@ -1,14 +1,12 @@
 @props(['errors'])
 
 @if ($errors->any())
-    <div {{ $attributes }}>
-        <div class="font-medium text-red-600">
-            {{ __('Whoops! Something went wrong.') }}
-        </div>
-
-        <ul class="mt-3 list-disc list-inside text-sm text-red-600">
+    <div class="validation-errors"{{ $attributes }}>
+        <ul class="validation-errors__errors">
             @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
+                <li class="validation-errors__errors__error">
+                    {{ $error }}
+                </li>
             @endforeach
         </ul>
     </div>
