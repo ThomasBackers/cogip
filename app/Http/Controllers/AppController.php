2d9a7c610cd67be3a57a3f222ee2e5ac7efe7c17
Thomas Backers <thomasbackers@gmail.com>
2022-01-24 16:31:10 +0100

    checkpoint: dashboard foreach + design on its way

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -4,13 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Company;
+use App\Models\Contact;
+use App\Models\Invoice;
 
 class AppController extends Controller
 {
     public function index()
     {
+        $rawQuery = 'coalesce(updated_at, created_at) DESC';
+
         return view('dashboard')
-            ->with('companies', Company::orderByRaw('coalesce(updated_at, created_at) DESC')
+            ->with('companies', Company::orderByRaw($rawQuery)
+                ->limit(5)
+                ->get())
+            ->with('contacts', Contact::orderByRaw($rawQuery)
+                ->limit(5)
+                ->get())
+            ->with('invoices', Invoice::orderByRaw($rawQuery)
                 ->limit(5)
                 ->get());
     }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,6 +5,53 @@
         </h1>
     </x-slot>
 
+    <section class="contacts">
+        <h2 class="contacts__heading">
+            <i class="fas fa-building"></i> last contacts
+        </h2>
+
+        @foreach ($contacts as $contact)
+            <div class="contact">
+                <h3 class="contact__name">
+                    {{ $contact->firstname }}
+                    {{ $contact->lastname }}
+                </h3>
+
+                <ul class="contact__data-list">
+                    <li class="contact__data-list__element">
+                        <h4 class="contact__data-list__element__heading">
+                            company:
+                        </h4>
+
+                        <p class="contact__data-list__element__data">
+                            {{ $contact->company->name }}
+                        </p>
+                    </li>
+
+                    <li class="contact__data-list__element">
+                        <h4 class="company__data-list__element__heading">
+                            email:
+                        </h4>
+
+                        <p class="contact__data-list__element__data">
+                            {{ $contact->email }}
+                        </p>
+                    </li>
+
+                    <li class="contact__data-list__element">
+                        <h4 class="contact__data-list__element__heading">
+                            phone:
+                        </h4>
+
+                        <p class="contact__data-list__element__data">
+                            {{ $contact->phone_number }}
+                        </p>
+                    </li>
+                </ul>
+            </div>
+        @endforeach
+    </section>
+   
     <section class="companies">
         <h2 class="companies__heading">
             <i class="fas fa-building"></i> last companies
@@ -61,5 +108,5 @@
                 </ul>
             </div>
         @endforeach
-        </section>
+    </section>
 </x-app-layout>
