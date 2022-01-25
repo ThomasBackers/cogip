63330bebb66ee809f5b3c2871cf531a1cdf9e6f1
Thomas Backers <thomasbackers@gmail.com>
2022-01-25 16:13:14 +0100

    company details page done

--- resources/views/companies/show.blade.php
+++ resources/views/companies/show.blade.php
@@ -107,4 +107,68 @@
             </div>
         @endforeach
     </section>
+
+    <section class="invoices">
+        <h2 class="invoices__heading">
+            <i class="fas fa-file-invoice-dollar"></i> invoices
+        </h2>
+
+        @foreach($company->contacts as $contact)
+            @foreach ($contact->invoices as $invoice)
+                <div class="invoice">
+                    <h3 class="invoice__name">
+                        {{ $invoice->invoice_number }}
+                    </h3>
+
+                    <ul class="invoice__data-list">
+                        <li class="invoice__data-list__element">
+                            <h4 class="invoice__data-list__element__heading">
+                                company:
+                            </h4>
+
+                            <p class="invoice__data-list__element__data">
+                                {{ $invoice->contact->company->name }}
+                            </p>
+                        </li>
+
+                        <li class="invoice__data-list__element">
+                            <h4 class="invoice__data-list__element__heading">
+                                amount:
+                            </h4>
+
+                            <p class="invoice__data-list__element__data">
+                                {{ $invoice->amount }}€
+                            </p>
+                        </li>
+
+                        <li class="invoice__data-list__element">
+                            <h4 class="invoice__data-list__element__heading">
+                                status:
+                            </h4>
+
+                            <p class="invoice__data-list__element__data">
+                                @if ($invoice->outstanding_balance == 0)
+                                    paid
+                                @else
+                                    pending
+                                @endif
+                            </p>
+                        </li>
+
+                        @if ($invoice->outstanding_balance != 0)
+                            <li class="invoice__data-list__element">
+                                <h4 class="invoice__data-list__element__heading">
+                                    outstanding balance:
+                                </h4>
+
+                                <p class="invoice__data-list__element__data">
+                                    {{ $invoice->outstanding_balance }}€
+                                </p>
+                            </li>
+                        @endif
+                    </ul>
+                </div>
+            @endforeach
+        @endforeach
+    </section>
 </x-app-layout>
