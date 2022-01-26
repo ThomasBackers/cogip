62d2896cfce735026930e55f9b8a9c426f6287b0
Thomas Backers <thomasbackers@gmail.com>
2022-01-26 16:25:19 +0100

    checkpoint before editing form

---
+++ resources/views/edit/form.blade.php
@@ -0,0 +1,14 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h1 class="header__heading">
+            {{ __('Editing') }}
+        </h1>
+    </x-slot>
+
+    <section class="editing">
+        <form action="" class="editing__form">
+            @csrf
+            @method('PUT')
+        </form>
+    </section>
+</x-app-layout>
