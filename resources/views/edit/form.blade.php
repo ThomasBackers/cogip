99e5e3470ff4a23eacd5be044c9a0f1210ca84f8
Thomas Backers <thomasbackers@gmail.com>
2022-01-27 12:42:51 +0100

    checkpoint before a break

--- resources/views/edit/form.blade.php
+++ resources/views/edit/form.blade.php
@@ -25,7 +25,133 @@
                     value="{{ $data->name }}"
                 @endif
                 >
+
+                <input
+                class="editing__form__input"
+                type="text"
+                name="address"
+                @if ($type === 'Editing')
+                    value="{{ $data->address }}"
+                @endif
+                >
+
+                <input
+                class="editing__form__input"
+                type="text"
+                name="number"
+                min="1000"
+                max="99999"
+                step="1"
+                @if ($type === 'Editing')
+                    value="{{ $data->zip_code }}"
+                @endif
+                >
+
+                <input
+                class="editing__form__input"
+                type="text"
+                name="city"
+                @if ($type === 'Editing')
+                    value="{{ $data->city }}"
+                @endif
+                >
+
+                <input
+                class="editing__form__input"
+                type="text"
+                name="country"
+                @if ($type === 'Editing')
+                    value="{{ $data->country }}"
+                @endif
+                >
+
+                <input
+                class="editing__form__input"
+                type="text"
+                name="vat_number"
+                @if ($type === 'Editing')
+                    value="{{ $data->vat_number }}"
+                @endif
+                >
+
+                <input
+                class="editing__form__input"
+                type="text"
+                name="name"
+                @if ($type === 'Editing')
+                    value="{{ $data->name }}"
+                @endif
+                >
+
+                <select
+                class="editing__form__input"
+                name="category"
+                >
+                    <option
+                    value="client"
+                    @if ($data->category === 'client')
+                        selected="selected"
+                    @endif
+                    >
+                        Client
+                    </option>
+
+                    <option
+                    value="supplier"
+                    @if ($data->category === 'supplier')
+                        selected="selected"
+                    @endif
+                    >
+                        Supplier
+                    </option>
+                </select>
+
             @elseif ($group === 'contacts')
+                <input
+                class="editing__form__input"
+                type="text"
+                name="firstname"
+                @if ($type === 'Editing')
+                    value="{{ $data->firstname }}"
+                @endif
+                >
+
+                <input
+                class="editing__form__input"
+                type="text"
+                name="lastname"
+                @if ($type === 'Editing')
+                    value="{{ $data->lastname }}"
+                @endif
+                >
+
+                <input
+                class="editing__form__input"
+                type="text"
+                name="company"
+                @if ($type === 'Editing')
+                    value="{{ $data->company->name }}"
+                @endif
+                >
+
+                <input
+                class="editing__form__input"
+                type="email"
+                name="email"
+                @if ($type === 'Editing')
+                    value="{{ $data->email }}"
+                @endif
+                >
+
+                <input
+                class="editing__form__input"
+                type="tel"
+                name="phone_number"
+                pattern="[0-9]{10}"
+                @if ($type === 'Editing')
+                    value="{{ $data->phone_number }}"
+                @endif
+                >
 
             @elseif ($group === 'invoices')
 
